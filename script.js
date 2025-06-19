772943cf8a3c77d03ca6b08d7135e72f99dfe4c1
ejcents <mdc.ejcentillas@gmail.com>
2025-06-19 20:32:57 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Scroll reveal or form alerts, if needed
+console.log('United Riders Charity Club Loaded');
+
+// Hamburger toggle
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
